b570d3d806181f90fe79fa61946aa7da0b9800ce
philippark <philippark271@gmail.com>
2024-06-27 21:56:55 -0400

    delete key, does nothing for now

--- kilo.c
+++ kilo.c
@@ -21,7 +21,8 @@ enum editorKey {
   PAGE_UP,
   PAGE_DOWN,
   HOME_KEY,
-  END_KEY
+  END_KEY,
+  DEL_KEY
 };
 
 /**data */
@@ -91,6 +92,7 @@ int editorReadKey(){
         if (seq[2] == '~'){
           switch(seq[1]){
             case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
             case '4': return END_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;

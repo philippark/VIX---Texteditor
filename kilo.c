dcbf30145f5bdca0864fa49fe5d8452b5c3a955d
philippark <philippark271@gmail.com>
2024-07-12 22:34:20 -0400

    Enter key

--- kilo.c
+++ kilo.c
@@ -230,10 +230,12 @@ void editorUpdateRow(erow* row){
 
 }
 
-void editorAppendRow(char *s, size_t len){
-  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+void editorInsertRow(int at, char *s, size_t len){
+  if (at < 0 || at > E.numrows) return;
 
-  int at = E.numrows;
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at+1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
   E.row[at].size = len;
   E.row[at].chars = malloc(len+1);
   memcpy(E.row[at].chars, s, len);
@@ -269,20 +271,65 @@ void editorRowDeleteChar(erow *row, int at){
   E.dirty++;
 }
 
+void editorFreeRow(erow *row){
+  free(row->render);
+  free(row->chars);
+}
+
+void editorDelRow(int at){
+  if (at < 0 || at >= E.numrows) return;
+  editorFreeRow(&E.row[at]);
+  memmove(&E.row[at], &E.row[at+1], sizeof(erow) * (E.numrows - at - 1));
+  E.numrows--;
+  E.dirty++;
+}
+
+void editorRowAppendString(erow *row, char* s, size_t len){
+  row->chars = realloc(row->chars, row->size + len + 1);
+  memcpy(&row->chars[row->size], s, len);
+  row->size += len;
+  row->chars[row->size] = '\0';
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
 void editorDelChar(){
   if (E.cy == E.numrows) return;
+  if (E.cx == 0 && E.cy == 0) return;
 
   erow *row = &E.row[E.cy];
   if (E.cx > 0){
     editorRowDeleteChar(row, E.cx-1);
     E.cx--;
   }
+  else{
+    E.cx = E.row[E.cy-1].size;
+    editorRowAppendString(&E.row[E.cy-1], row->chars, row->size);
+    editorDelRow(E.cy);
+    E.cy--;
+  }
+}
+
+void editorInsertNewline(){
+  if (E.cx == 0){
+    editorInsertRow(E.cy, "", 0);
+  }
+  else{
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
 }
 
 /**editor operations */
 void editorInsertChar(int c){
   if (E.cy == E.numrows){
-    editorAppendRow("", 0);
+    editorInsertRow(E.numrows, "", 0);
   }
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
@@ -326,7 +373,7 @@ void editorOpen(char* filename){
       linelen--;
     }
 
-    editorAppendRow(line, linelen);
+    editorInsertRow(E.numrows, line, linelen);
   }
 
   free(line);
@@ -359,7 +406,6 @@ void editorSave(){
 }
 
 
-/** append buffer */
 struct abuf {
   char* b;
   int len;
@@ -566,6 +612,7 @@ void editorProcessKeypress() {
 
   switch (c) {
     case '\r':
+      editorInsertNewline();
       break;
 
     case CTRL_KEY('q'):
